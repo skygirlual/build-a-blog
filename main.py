679a254e7fa24b0b0d0ec8942b92b5b3136aabb9
skygirlual <33335620+skygirlual@users.noreply.github.com>
2018-01-08 09:12:17 -0600

    Nearly there.
    Errors working.  post redirects to blog.  blog lists in desc order.

--- main.py
+++ main.py
@@ -24,12 +24,14 @@ def index():
 
 @app.route('/blog', methods=['POST', 'GET'])
 def blogposts():
-    allPosts = Blog.query.all()
+    #TO DO  change order to desc by id for newer posts on top.
+    allPosts = Blog.query.order_by(Blog.id.desc()).all()
+    #allPosts = Blog.query.all()
     return render_template('blogposts.html',title="My Fantastic Blog", posts=allPosts)
 
 #@app.route('/newpost', methods=['POST', 'GET'])
 @app.route('/newpost', methods=['POST', 'GET'])
-def new_post():
+def newPost():
 
     if request.method == 'POST':
         #post_id = int(request.form['id'])
@@ -40,23 +42,23 @@ def new_post():
         checklist =[post_title, post_body]
         title_error = ''
         body_error = ''
-        for i in checklist:
-                if len(post_title) <1 or len(post_title) > 120:
-                    title_error = "Post must have a title"
-                    return render_template('newpost.html', post_body = post_body, title_error = title_error)
-                if len(post_body) <1 or len(post_body) > 520:
-                    body_error = "You forgot the body"
-                    return render_template('newpost.html', post_title = post_title, body_error = body_error)
+        for item in checklist:
+            if len(post_title) <1 or len(post_title) > 120:
+                title_error = "Post must have a title."
+                return render_template('newpost.html', post_body = post_body, title_error = title_error)
+
+            if len(post_body) <1 or len(post_body) > 520:
+                body_error = "You forgot the body."
+                return render_template('newpost.html', post_title = post_title, body_error = body_error)
 
         if not title_error and not body_error:
             #post_id = Blog(post_id... # Removed post_id uneeded.        
             new_post = Blog(post_title, post_body)
             db.session.add(new_post)
             db.session.commit()
-            return render_template('newpost.html')
+            return redirect('/blog')
 
     else:
-        #return redirect('/blog')
         return render_template('newpost.html')
 
 if __name__ == '__main__':
