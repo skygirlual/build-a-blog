71fdd8b8149310eddda67ba154c018a4a36cdf77
skygirlual <33335620+skygirlual@users.noreply.github.com>
2018-01-08 03:26:20 -0600

    getting there.
    Error messages are working.
    Database working.

---
+++ main.py
@@ -0,0 +1,63 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:password@localhost:3306/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(510))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+
+@app.route('/')
+def index():
+    return redirect ('/blog')
+
+@app.route('/blog', methods=['POST', 'GET'])
+def blogposts():
+    allPosts = Blog.query.all()
+    return render_template('blogposts.html',title="My Fantastic Blog", posts=allPosts)
+
+#@app.route('/newpost', methods=['POST', 'GET'])
+@app.route('/newpost', methods=['POST', 'GET'])
+def new_post():
+
+    if request.method == 'POST':
+        #post_id = int(request.form['id'])
+        post_title = request.form['title']
+        post_body = request.form['body']
+        
+        # input validation
+        checklist =[post_title, post_body]
+        title_error = ''
+        body_error = ''
+        for i in checklist:
+                if len(post_title) <1 or len(post_title) > 120:
+                    title_error = "Post must have a title"
+                    return render_template('newpost.html', post_body = post_body, title_error = title_error)
+                if len(post_body) <1 or len(post_body) > 520:
+                    body_error = "You forgot the body"
+                    return render_template('newpost.html', post_title = post_title, body_error = body_error)
+
+        if not title_error and not body_error:
+            #post_id = Blog(post_id... # Removed post_id uneeded.        
+            new_post = Blog(post_title, post_body)
+            db.session.add(new_post)
+            db.session.commit()
+            return render_template('newpost.html')
+
+    else:
+        #return redirect('/blog')
+        return render_template('newpost.html')
+
+if __name__ == '__main__':
+    app.run()
