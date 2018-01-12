1cdeb7719e80dd289f42079318dbccdc59567a3a
skygirlual <skygirlual@hotmail.com>
2018-01-12 13:59:58 -0600

    redirect invalid post numbers to blog.

--- main.py
+++ main.py
@@ -29,21 +29,19 @@ def allblogposts():
 
 @app.route('/post/<int:post_id>', methods=['POST', 'GET'])
 def show_post(post_id):
-    #postId = oneBlogPost.request.get("id")
-    #, form_value = request.args.get(Blog.id)
+    max_post_id = db.session.query(db.func.max(Blog.id)).scalar()
+    if post_id == 0 or post_id > max_post_id:
+            return redirect ('/blog')
+
     onePost = Blog.query.filter(Blog.id==post_id).all()
     title_post=onePost[0].title
-    #id = onePost.key().id()
-    #self.redirect("/post" postId=id)
 
     return render_template('post.html', posts=onePost, post_id=post_id)
 
-#@app.route('/newpost', methods=['POST', 'GET'])
 @app.route('/newpost', methods=['POST', 'GET'])
 def newPost():
 
     if request.method == 'POST':
-        #post_id = int(request.form['id'])
         post_title = request.form['title']
         post_body = request.form['body']
         
@@ -61,18 +59,12 @@ def newPost():
                 return render_template('newpost.html', post_title = post_title, body_error = body_error)
 
         if not title_error and not body_error:
-            #post_id = Blog(post_id... # Removed post_id uneeded.        
             new_post = Blog(post_title, post_body)
             db.session.add(new_post)
             db.session.commit()
             new_post_id = db.session.query(db.func.max(Blog.id)).scalar()
-
-            #post_id = db.func.max(Blog.id)
             onePost = Blog.query.filter(Blog.id==new_post_id).all()
-            
-            
             return redirect('/post/%s' % new_post_id)
-            #return redirect('/blog')
 
     else:
         return render_template('newpost.html')
